a1f710d29aa6bedb30e5d8c97e6d02183bf5aac0
DixitJain546 <dixit.jain0002@gmail.com>
2021-01-20 19:33:15 +0530

    qty removed

--- app/src/main/java/com/example/unisole/adapters/CartAdapter.java
+++ app/src/main/java/com/example/unisole/adapters/CartAdapter.java
@@ -4,7 +4,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -59,7 +58,6 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.viewHolder> {
 
         public class viewHolder extends RecyclerView.ViewHolder {
         View rootView;
-        ImageView productImage;
         TextView productName;
         TextView productPrice;
         Button remove;
